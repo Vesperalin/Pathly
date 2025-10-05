fcd4f0db4db99797d049fb78d0512fa37b599293
Klaudia Błażyczek <klaudia.blazyczek@avenga.com>
2025-10-05 20:57:28 +0200

    implement

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,53 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { includeIgnoreFile } from "@eslint/compat";
+import eslint from "@eslint/js";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import prettier from "eslint-plugin-prettier/recommended";
+import react from "eslint-plugin-react";
+import reactCompiler from "eslint-plugin-react-compiler";
+import reactHooks from "eslint-plugin-react-hooks";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import tseslint from "typescript-eslint";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+export default tseslint.config(
+  includeIgnoreFile(gitignorePath),
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // Base JS and TS strict configs
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+
+  // Plugins and rules
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      "jsx-a11y": jsxA11y,
+      "react-compiler": reactCompiler,
+    },
+    rules: {
+      ...react.configs.flat.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      ...jsxA11y.flatConfigs.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+      "react-compiler/react-compiler": "error",
+      "no-console": "warn",
+      "no-unused-vars": "off",
+    },
+    languageOptions: {
+      ...react.configs.flat.recommended.languageOptions,
+      globals: {
+        window: true,
+        document: true,
+      },
+    },
+    settings: { react: { version: "detect" } },
   },
-];
 
-export default eslintConfig;
+  // Prettier last
+  prettier
+);
