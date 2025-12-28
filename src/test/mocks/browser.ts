aae2747b275c221a0ed32729489c450993ec45f8
Vesperalin <blazyczek.klaudia@gmail.com>
2025-12-28 13:02:14 +0100

    add prject setup

--- src/test/mocks/browser.ts
+++
@@ -1,5 +0,0 @@
-import { setupWorker } from "msw/browser";
-import { handlers } from "./handlers";
-
-// Setup worker for browser environment (development/debugging)
-export const worker = setupWorker(...handlers);
